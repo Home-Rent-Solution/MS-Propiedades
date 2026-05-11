da03b80ef11e5de4ceae9738cb19ab3f9fa02604
victorurravalenzuela <vi.urrav@duocuc.cl>
2026-05-11 16:26:06 -0400

    Se completa implementacion de DTO: - Creado PropiedadesResponseDTO para desacoplar la respuesta JSON del modelo de entidad. - Asegurada la exposición controlada de campos (id, disponible, etc.) hacia el cliente.

--- src/main/java/com/home_rental_solution/ms_propiedades/dto/PropiedadesResponseDTO.java
+++ src/main/java/com/home_rental_solution/ms_propiedades/dto/PropiedadesResponseDTO.java
@@ -1,4 +1,23 @@
 package com.home_rental_solution.ms_propiedades.dto;
 
+import com.home_rental_solution.ms_propiedades.model.Propiedades;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PropiedadesResponseDTO {
+
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private String ubicacion;
+    private BigDecimal precio;
+    private Integer idAnfitrion;
+    private Propiedades.TipoPropiedad tipo;
+    private boolean disponible;
 }
