30dadeb1aafd419ce3cf97f998acdccd20ef6be2
victorurravalenzuela <vi.urrav@duocuc.cl>
2026-05-11 20:16:22 -0400

    Se desacopla capa de persistencia mediante DTO: - Migrados todos los métodos para retornar PropiedadesResponseDTO. - Implementados métodos privados de mapeo (Entity <-> DTO). - Reemplazadas excepciones genéricas por RuntimeException para integración con Advice. - Optimizada la lógica de actualización en el método editar.

--- src/main/java/com/home_rental_solution/ms_propiedades/service/PropiedadesService.java
+++ src/main/java/com/home_rental_solution/ms_propiedades/service/PropiedadesService.java
@@ -1,5 +1,7 @@
 package com.home_rental_solution.ms_propiedades.service;
 
+import com.home_rental_solution.ms_propiedades.dto.PropiedadesRequestDTO;
+import com.home_rental_solution.ms_propiedades.dto.PropiedadesResponseDTO;
 import com.home_rental_solution.ms_propiedades.model.Propiedades;
 import com.home_rental_solution.ms_propiedades.repository.PropiedadesRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.math.BigDecimal;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,92 +17,115 @@ public class PropiedadesService {
 
     private final PropiedadesRepository propiedadesRepository;
 
+    //Mapeo Entidad -> ResponseDTO
+    private PropiedadesResponseDTO mapToDTO(Propiedades propiedad){
+        return new PropiedadesResponseDTO(
+                propiedad.getIdPropiedad(),
+                propiedad.getTitulo(),
+                propiedad.getDescripcion(),
+                propiedad.getUbicacion(),
+                propiedad.getPrecio(),
+                propiedad.getIdAnfitrion(),
+                propiedad.getTipo(),
+                propiedad.isDisponible()
+        );
+    }
+
+    //Mapeo RequestDTO -> Entidad
+    private Propiedades mapToEntity(PropiedadesRequestDTO dto){
+        return new Propiedades(
+                null,
+                dto.getNombre(),
+                dto.getDescripcion(),
+                dto.getUbicacion(),
+                dto.getPrecio(),
+                dto.getIdAnfitrion(),
+                dto.getTipo(),
+                false
+        );
+    }
+
     //***CRUD**
     //GET /propiedades
-    public List<Propiedades> mostrarPropiedades(){
-        return propiedadesRepository.findAll();
+    public List<PropiedadesResponseDTO> mostrarPropiedades(){
+        return propiedadesRepository.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
     //GET /propiedades/id
-    public Propiedades mostrarPorId(int id){
-        return propiedadesRepository.findById(id).orElse(null);
+    public PropiedadesResponseDTO mostrarPorId(int id){
+        Propiedades propiedad = propiedadesRepository.findById(id).orElseThrow(() -> new RuntimeException("La propiedad " +
+                "con ID: " + id + " no existe"));
+        return mapToDTO(propiedad);
     }
 
     //POST /propiedades
-    public Propiedades save (Propiedades nuevaPropiedad) throws Exception{
-        if (nuevaPropiedad.getPrecio() == null || nuevaPropiedad.getPrecio().compareTo(BigDecimal.ZERO) <= 0){
-            throw new Exception("El precio debe ser mayor a 0");
-        }
-        return propiedadesRepository.save(nuevaPropiedad);
+    public PropiedadesResponseDTO save (PropiedadesRequestDTO dto){
+        Propiedades nuevaPropiedad = mapToEntity(dto);
+        return mapToDTO(propiedadesRepository.save(nuevaPropiedad));
     }
 
     //PUT /propiedades/Id
-    public Propiedades editar(int id, Propiedades propiedadEditada) throws Exception{
-        Propiedades propiedadExistente = propiedadesRepository.findById(id).orElse(null);
-        if (propiedadExistente == null){
-            throw new Exception("La propiedad con ID: " + id + " no existe");
-        }
-        if (propiedadEditada.getPrecio() == null || propiedadEditada.getPrecio().compareTo(BigDecimal.ZERO) <= 0){
-            throw  new Exception("El precio debe ser mayor a 0");
-        }
-        propiedadEditada.setIdPropiedad(id);
-        propiedadEditada.setDisponible(propiedadExistente.isDisponible());
-        return propiedadesRepository.save(propiedadEditada);
+    public PropiedadesResponseDTO editar(int id, PropiedadesRequestDTO dto) {
+        Propiedades propiedadExistente = propiedadesRepository.findById(id).orElseThrow(() -> new RuntimeException("La " +
+                "propiedad con ID: " + id + " no existe"));
+        propiedadExistente.setTitulo(dto.getNombre());
+        propiedadExistente.setDescripcion(dto.getDescripcion());
+        propiedadExistente.setUbicacion(dto.getUbicacion());
+        propiedadExistente.setPrecio(dto.getPrecio());
+        propiedadExistente.setIdAnfitrion(dto.getIdAnfitrion());
+        propiedadExistente.setTipo(dto.getTipo());
+        // disponible no se toca porque se maneja con PUT
+        return mapToDTO(propiedadesRepository.save(propiedadExistente));
     }
 
     // DELETE /propiedad/id
-    public void borrar (int id) throws Exception{
+    public void borrar (int id){
         if (!propiedadesRepository.existsById(id)){
-            throw new Exception("La propiedad con ID: " + id + " no existe");
+            throw new RuntimeException("La propiedad con ID: " + id + " no existe");
         }
         propiedadesRepository.deleteById(id);
     }
 
     //***EXTRAS***
-    //GET /propiedades/usuario/id
-    public List<Propiedades> mostrarPorAnfitrion(int idAnfitrion){
-        return propiedadesRepository.findByIdAnfitrion(idAnfitrion);
+    //GET /propiedades/anfitrion/id
+    public List<PropiedadesResponseDTO> mostrarPorAnfitrion(int idAnfitrion){
+        return propiedadesRepository.findByIdAnfitrion(idAnfitrion).stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
     //GET /propiedades/precio
-    public List<Propiedades> mostrarPorPrecio(BigDecimal min, BigDecimal max) throws Exception{
-        if (min == null || max == null){
-            throw new Exception("Debe ingresar ambos valores de precio");
-        }
+    public List<PropiedadesResponseDTO> mostrarPorPrecio(BigDecimal min, BigDecimal max){
         if (min.compareTo(max) > 0){
-            throw  new Exception("El precio minimo no puede ser mayor que el precio maximo");
+            throw new RuntimeException("El precio minimo no puede ser mayor que el maximo");
         }
-        return propiedadesRepository.findByPrecioBetween(min, max);
+        return propiedadesRepository.findByPrecioBetween(min, max).stream().map(this::mapToDTO).collect(Collectors
+                .toList());
     }
 
     //GET /propiedades/tipo
-    public List<Propiedades> mostrarPorTipo(Propiedades.TipoPropiedad tipo){
-        return propiedadesRepository.findByTipo(tipo);
+    public List<PropiedadesResponseDTO> mostrarPorTipo(Propiedades.TipoPropiedad tipo){
+        return propiedadesRepository.findByTipo(tipo).stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
     //GET /propiedades/buscar?ubicacion=&precioMin=&precioMax=
-    public List<Propiedades> mostrarUbicacionPrecio(String ubicacion, BigDecimal precioMin, BigDecimal precioMax) throws Exception{
-        if (precioMin == null || precioMax == null){
-            throw new Exception("Debe ingresar ambos valores de precio");
-        }
+    public List<PropiedadesResponseDTO> mostrarUbicacionPrecio(String ubicacion, BigDecimal precioMin,
+                                                               BigDecimal precioMax){
         if (precioMin.compareTo(precioMax) > 0){
-            throw new Exception("El precio minimo no puede ser mayor que el precio maximo");
+            throw new RuntimeException("El precio minimo no debe ser mayor que el maximo");
         }
-        return propiedadesRepository.findByUbicacionContainingIgnoreCaseAndPrecioBetween(ubicacion, precioMin, precioMax);
+        return propiedadesRepository.findByUbicacionContainingIgnoreCaseAndPrecioBetween(ubicacion, precioMin, precioMax)
+                .stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
     //GET /propiedades/disponibles
-    public List<Propiedades> mostrarDisponibles(){
-        return propiedadesRepository.findByDisponibleTrue();
+    public List<PropiedadesResponseDTO> mostrarDisponibles(){
+        return propiedadesRepository.findByDisponibleTrue().stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
-    //PUT /propiedades/estado
-    public Propiedades cambiarEstado(int id) throws Exception{
-        Propiedades propiedad = propiedadesRepository.findById(id).orElse(null);
-        if (propiedad ==  null){
-            throw new Exception("La propiedad con ID: " + id + " no existe");
-        }
+    //PUT /propiedades/id/estado
+    public PropiedadesResponseDTO cambiarEstado(int id){
+        Propiedades propiedad = propiedadesRepository.findById(id).orElseThrow(() -> new RuntimeException("La propiedad " +
+                "con ID: " + id + " no existe"));
         propiedad.setDisponible(!propiedad.isDisponible());
-        return propiedadesRepository.save(propiedad);
+        return mapToDTO(propiedadesRepository.save(propiedad));
     }
 }
