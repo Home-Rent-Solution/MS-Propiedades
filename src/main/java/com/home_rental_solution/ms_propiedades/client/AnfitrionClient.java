998d46c857477cc4feb72a577605f66c8304d6c7
victorurravalenzuela <vi.urrav@duocuc.cl>
2026-06-18 12:56:32 -0400

    Se modifica para implementar eureka

--- src/main/java/com/home_rental_solution/ms_propiedades/client/AnfitrionClient.java
+++ src/main/java/com/home_rental_solution/ms_propiedades/client/AnfitrionClient.java
@@ -5,8 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
-        name="ms-anfitriones",
-        url ="${ms.anfitriones.url}"
+        name="ms-anfitriones"
 )
 public interface AnfitrionClient {
 
